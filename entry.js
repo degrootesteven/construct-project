c54e743cffbe2a1cba3878e29dc847dae7e064ba
degrootesteven <109435742+degrootesteven@users.noreply.github.com>
2025-09-09 11:59:06 -0400

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
-class Entry{
-  constructor(sel, ver, sH){
+class Entry {
+  constructor(sel, ver, sH) {
     this.sel = sel;
     this.ver = ver;
 
@@ -14,56 +14,63 @@ class Entry{
 
     this.sH = sH;
 
-    this.pg;
-    if(ver == 5){
+    this.pg = null;
+    if (ver === 5) {
       this.pg = new Cloud(heightRatio[this.sel], this.sH, this.sel);
-    } else if(ver == 3){
+    } else if (ver === 3) {
       this.pg = new Scribble(heightRatio[this.sel], this.sH, this.sel);
-    } else if(ver == 6){
+    } else if (ver === 6) {
       this.pg = new Zigzag(heightRatio[this.sel], this.sH, this.sel);
-    } else if(ver == 7){
+    } else if (ver === 7) {
       this.pg = new Grad(heightRatio[this.sel], this.sH, this.sel);
-    } else if(ver == 8){
+    } else if (ver === 8) {
       this.pg = new Box(heightRatio[this.sel], this.sH, this.sel);
     }
   }
 
-  display(){
-    // seperate pieces
-    if(this.ver == 3 || this.ver == 5 || this.ver == 6 || this.ver == 7 || this.ver == 8){
+  display() {
+    // Shapes (cloud, scribble, zigzag, gradient, box)
+    if (this.ver === 3 || this.ver === 5 || this.ver === 6 || this.ver === 7 || this.ver === 8) {
       push();
-        translate(heightRatio[this.sel]/2, 0);
-        this.pg.display();
+      translate(heightRatio[this.sel] / 2, 0);
+      this.pg.display();
       pop();
-    // typography
-    } else{
-      if(this.ver == 9){
+
+    // GIFs
+    } else if (this.ver === 0) {
+      const gifIndex = this.sel % pImg.length; // cycle through available GIFs
+      if (pImg[gifIndex]) {
+        image(pImg[gifIndex], 0, -this.sH / 2, heightRatio[this.sel], this.sH);
+      }
+
+    // Typography
+    } else {
+      if (this.ver === 9) {
         this.update();
         shearX(this.shr);
       }
-      image(pgT[this.sel], 0, -this.sH/2, heightRatio[this.sel], this.sH);
+      image(pgT[this.sel], 0, -this.sH / 2, heightRatio[this.sel], this.sH);
     }
   }
 
-  update(){
-    if(this.ticker == this.animWindow){
+  update() {
+    if (this.ticker === this.animWindow) {
       this.currentShear = this.targetShear;
       this.newShear();
       this.ticker = 0;
     }
 
-    var animTicker = map(this.ticker, 0, this.animWindow, 0, 1);
-    var animValue = map(aSet(animTicker, 8), 0, 1, this.currentShear, this.targetShear);
+    const animTicker = map(this.ticker, 0, this.animWindow, 0, 1);
+    const animValue = map(aSet(animTicker, 8), 0, 1, this.currentShear, this.targetShear);
 
     this.shr = animValue;
-
-    this.ticker ++;
+    this.ticker++;
   }
 
-  newShear(){
-    var shearOn = random(10);
-    if(shearOn > 7.5){
-      this.targetShear = random(-PI/3, PI/3);
+  newShear() {
+    const shearOn = random(10);
+    if (shearOn > 7.5) {
+      this.targetShear = random(-PI / 3, PI / 3);
     } else {
       this.targetShear = 0;
     }
