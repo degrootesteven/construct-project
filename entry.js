eb76ff65c94f9b1f23fa458f70a359907fddd0b7
degrootesteven <109435742+degrootesteven@users.noreply.github.com>
2025-09-09 11:38:40 -0400

    Update entry.js

--- entry.js
+++ entry.js
@@ -28,36 +28,22 @@ class Entry{
     }
   }
 
-  // entry.js
-display(){
-if (this.ver === 0) {
-  const w = heightRatio[this.sel];
-  const h = this.sH;
-  const idx = gifIndex[this.sel];
-
-  if (gifEls[idx] && gifEls[idx].elt && gifEls[idx].elt.complete) {
-    drawingContext.drawImage(gifEls[idx].elt, 0, -h/2, w, h);
-  } else {
-    image(pgT[this.sel], 0, -h/2, w, h);
+  display(){
+    // seperate pieces
+    if(this.ver == 3 || this.ver == 5 || this.ver == 6 || this.ver == 7 || this.ver == 8){
+      push();
+        translate(heightRatio[this.sel]/2, 0);
+        this.pg.display();
+      pop();
+    // typography
+    } else{
+      if(this.ver == 9){
+        this.update();
+        shearX(this.shr);
+      }
+      image(pgT[this.sel], 0, -this.sH/2, heightRatio[this.sel], this.sH);
+    }
   }
-  return;
-}
-
-
-
-  // ... keep your existing branches
-  if (this.ver===3 || this.ver===5 || this.ver===6 || this.ver===7 || this.ver===8){
-    push();
-    translate(heightRatio[this.sel]/2, 0);
-    if (typeof SHAPE_SCALE !== 'undefined') scale(SHAPE_SCALE, SHAPE_SCALE);
-    this.pg.display();
-    pop();
-  } else {
-    if (this.ver===9){ this.update(); shearX(this.shr); }
-    image(pgT[this.sel], 0, -this.sH/2, heightRatio[this.sel], this.sH);
-  }
-}
-
 
   update(){
     if(this.ticker == this.animWindow){
